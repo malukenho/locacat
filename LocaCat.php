484b0136fedc866980a6a32f1850977a8428792a
Jefersson Nathan <admin@phpse.net>
2013-07-02 23:11:27 -0300

    Implement help command and init a router this

--- LocaCat.php
+++ LocaCat.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 class Phergie_Plugin_LocaCat extends Phergie_Plugin_Abstract
 {
     public function onPrivmsg()
@@ -8,8 +8,53 @@ class Phergie_Plugin_LocaCat extends Phergie_Plugin_Abstract
         $message = $event->getText();
         
         if (preg_match('/^!locacat/i', $message)) {
-            $nick = $this->event->getNick();
-            $this->doNotice($nick, "Comando locacat foi executado...\n");
+            $message = ltrim($message, '!locacat');
+            $this->defineAction($message);
         }
     }
+    
+     
+    protected function defineAction($commands)
+    {
+        
+        $nick = $this->event->getNick();
+        
+        $action = array(
+            '-ls' => 'list', //@TODO: lista categorias
+            '-ta' => 'talk', //@TODO: texto aleatório de alguam categoria
+            '-hp' => 'help', // @TODO: apresenta o help
+            '-ad' => 'add'   // @TODO: Adciona um texto a uma categoria
+        );
+        
+        $command = substr(trim($commands), 0, 3);
+        
+        if (! $commands or ! array_key_exists($command, $action)) {
+            $this->displayHelp($nick);
+            return true;
+        }
+        
+        
+        $nick = $this->event->getNick();
+        $this->doNotice($nick, $command);
+    }
+    
+    private function help()
+    {
+        $output[] = "Bem vindo ao LocaCat Boot v0.1";
+        $output[] = "Comandos:";
+        $output[] = " ";
+        $output[] = "-ls: Exibi lista de categorias cadastradas no boot";
+        $output[] = "-ta: Exibe um texto aleatorio quando seguido da categoria";
+        $output[] = "-hp: Exibe esta tela";
+        return $output;
+    }
+    
+    public function displayHelp($nick)
+    {
+        foreach($this->help() as $value) {
+            $this->doNotice($nick, $value);
+        }
+        return true;
+    }
+    
 }
