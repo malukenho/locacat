ad33f4ded02f08243c5eaf60611cdb8dd69a335e
Jefersson Nathan <admin@phpse.net>
2013-07-04 18:28:51 -0300

    Add categories implementation and PHPdoc block

--- LocaCat.php
+++ LocaCat.php
@@ -1,7 +1,26 @@
 <?php
 
+/**
+* Phergie_Plugin_LocaCat
+*
+* @uses     Phergie_Plugin_Abstract
+*
+* @category Phergie Plugin
+* @package  Phergie
+* @author   Jefersson Nathan <malukenho@phpse.net>
+* @license  GNU
+*
+*/
 class Phergie_Plugin_LocaCat extends Phergie_Plugin_Abstract
 {
+
+    /**
+     * onPrivmsg
+     * 
+     * @access public
+     *
+     * @return void Value.
+     */
     public function onPrivmsg()
     {
         $event = $this->getEvent();
@@ -12,43 +31,140 @@ class Phergie_Plugin_LocaCat extends Phergie_Plugin_Abstract
             $this->defineAction($message);
         }
     }
-    
-     
+
+    /**
+     * defineAction
+     * 
+     * @param mixed $commands passed to the locacat.
+     *
+     * @access protected
+     *
+     * @return bool Value.
+     */
     protected function defineAction($commands)
     {
         
         $nick = $this->event->getNick();
         
         $action = array(
-            '-ls' => 'list', //@TODO: lista categorias
-            '-ta' => 'talk', //@TODO: texto aleatório de alguam categoria
-            '-hp' => 'help', // @TODO: apresenta o help
-            '-ad' => 'add'   // @TODO: Adciona um texto a uma categoria
+            '-ls' => 'list',
+            '-ta' => 'talk',
+            '-hp' => 'help' 
+           // '-ad' => 'add'    @TODO: Adciona um texto a uma categoria
         );
         
         $command = substr(trim($commands), 0, 3);
-        
+        $params  = str_replace($command, '', $commands);
+                
         if (! $commands or ! array_key_exists($command, $action)) {
             $this->displayHelp($nick);
             return true;
         }
-        
-        
-        $nick = $this->event->getNick();
-        $this->doNotice($nick, $command);
+
+        $this->getCommand($command, $params);    
+
+       return true;
     }
     
+    //@TODO: Rename this method :S
+    private function getValue($params)
+    {
+    	$nick = $this->event->getNick();
+
+    	if(! $params)
+    	{
+    		$this->doNotice($nick, 'COMMAD: -ta expects a parameter <category_name>');
+    		return true;
+    	}
+    	
+    	$listOfCategories = $this->categories();
+
+    	$this->doNotice($nick, $listOfCategories[$params]);
+    }
+
+    /**
+     * help
+     * 
+     * @access private
+     *
+     * @return array
+     */
     private function help()
     {
         $output[] = "Bem vindo ao LocaCat Boot v0.1";
         $output[] = "Comandos:";
         $output[] = " ";
-        $output[] = "-ls: Exibi lista de categorias cadastradas no boot";
-        $output[] = "-ta: Exibe um texto aleatorio quando seguido da categoria";
-        $output[] = "-hp: Exibe esta tela";
+        $output[] = "-ls                  : Exibi lista de categorias cadastradas no boot";
+        $output[] = "-ta <category_name>  : Exibe um texto aleatorio quando seguido da categoria";
+        $output[] = "-hp                  : Exibe esta tela";
         return $output;
     }
-    
+
+    /**
+     * categories
+     * 
+     * Return a array with the categories 
+     *
+     * @access private
+     *
+     * @return mixed Value.
+     */
+    private function categories()
+    {
+    	return array(
+    		'gif'   => array('cat.gif', 'cat2.gif'),
+    		'p'     => array('Lorem ipsum', 'Lorem lorem y'),
+    		'fun'   => array('Casiudis !!!', 'Shungary'),
+    		'sites' => array('catgifpage.com', 'anotherpage.php')
+	    );
+    }
+
+    private function getCommand($command, $params)
+    {
+    	switch($command)
+        {
+        	case '-ls':
+        		$this->listCategories();
+        		break;
+
+        	case '-ta':
+        		$this->getValue(trim($params));
+        		break;
+        }
+
+        return true;
+    }
+
+    /**
+     * listCategories
+     * 
+     * List all categories in self::categories() method
+     * 
+     * @access public
+     *
+     * @return mixed Value.
+     */
+    public function listCategories()
+    {
+    	$nick       = $this->event->getNick();
+    	$categories = array_keys($this->categories());
+
+    	$this->doNotice($nick, '[ Categories ]');
+    	$this->doNotice($nick, implode(', ', $categories));
+    	$this->doNotice($nick, ' ');
+        
+        return true;
+    }
+
+    /**
+     * displayHelp
+     * 
+     * @param string $nick of user.
+     *
+     * @access public
+     *
+     * @return mixed Value.
+     */
     public function displayHelp($nick)
     {
         foreach($this->help() as $value) {
